d41feb66c48eec46697058a498157d467fa131b0
Xudong <xudong@xudong-ThinkPad-T410>
2016-03-09 11:03:06 -0500

    All done.

--- downloadFromKestrel.sh
+++ downloadFromKestrel.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+mkdir -p ./BreastCancerWisconsinDownsampled/Original/StrictExamples/User0/Testing/
+scp liu@kestrel.csr.uky.edu:/homes/liu/Codes/PrefForestLearnExperiments/BreastCancerWisconsinDownsampled/Original/StrictExamples/User0/Testing/test0 ./BreastCancerWisconsinDownsampled/Original/StrictExamples/User0/Testing/
+
+mkdir -p ./CarEvaluation/Original/StrictExamples/User0/Testing/
+scp liu@kestrel.csr.uky.edu:/homes/liu/Codes/PrefForestLearnExperiments/CarEvaluation/Original/StrictExamples/User0/Testing/test0 ./CarEvaluation/Original/StrictExamples/User0/Testing/
+
 mkdir -p ./CreditApprovalDownsampledFurther/Original/StrictExamples/User0/Testing/
 scp liu@kestrel.csr.uky.edu:/homes/liu/Codes/PrefForestLearnExperiments/CreditApprovalDownsampledFurther/Original/StrictExamples/User0/Testing/test0 ./CreditApprovalDownsampledFurther/Original/StrictExamples/User0/Testing/
+
+mkdir -p ./GermanCreditDownsampledFurther/Original/StrictExamples/User0/Testing/
+scp liu@kestrel.csr.uky.edu:/homes/liu/Codes/PrefForestLearnExperiments/GermanCreditDownsampledFurther/Original/StrictExamples/User0/Testing/test0 ./GermanCreditDownsampledFurther/Original/StrictExamples/User0/Testing/
 
 mkdir -p ./IonosphereDownsampledFurther/Original/StrictExamples/User0/Testing/
 scp liu@kestrel.csr.uky.edu:/homes/liu/Codes/PrefForestLearnExperiments/IonosphereDownsampledFurther/Original/StrictExamples/User0/Testing/test0 ./IonosphereDownsampledFurther/Original/StrictExamples/User0/Testing/
@@ -9,8 +18,17 @@ scp liu@kestrel.csr.uky.edu:/homes/liu/Codes/PrefForestLearnExperiments/Ionosphe
 mkdir -p ./MammographicMassDownsampled/Original/StrictExamples/User0/Testing/
 scp liu@kestrel.csr.uky.edu:/homes/liu/Codes/PrefForestLearnExperiments/MammographicMassDownsampled/Original/StrictExamples/User0/Testing/test0 ./MammographicMassDownsampled/Original/StrictExamples/User0/Testing/
 
+mkdir -p ./MushroomDownsampled/Original/StrictExamples/User0/Testing/
+scp liu@kestrel.csr.uky.edu:/homes/liu/Codes/PrefForestLearnExperiments/MushroomDownsampled/Original/StrictExamples/User0/Testing/test0 ./MushroomDownsampled/Original/StrictExamples/User0/Testing/
+
+mkdir -p ./NurseryDownsampledFurther/Original/StrictExamples/User0/Testing/
+scp liu@kestrel.csr.uky.edu:/homes/liu/Codes/PrefForestLearnExperiments/NurseryDownsampledFurther/Original/StrictExamples/User0/Testing/test0 ./NurseryDownsampledFurther/Original/StrictExamples/User0/Testing/
+
 mkdir -p ./SpectHeartDownsampledFurther/Original/StrictExamples/User0/Testing/
 scp liu@kestrel.csr.uky.edu:/homes/liu/Codes/PrefForestLearnExperiments/SpectHeartDownsampledFurther/Original/StrictExamples/User0/Testing/test0 ./SpectHeartDownsampledFurther/Original/StrictExamples/User0/Testing/
+
+mkdir -p ./TicTacToe/Original/StrictExamples/User0/Testing/
+scp liu@kestrel.csr.uky.edu:/homes/liu/Codes/PrefForestLearnExperiments/TicTacToe/Original/StrictExamples/User0/Testing/test0 ./TicTacToe/Original/StrictExamples/User0/Testing/
 
 mkdir -p ./VehicleDownsampledFurther/Original/StrictExamples/User0/Testing/
 scp liu@kestrel.csr.uky.edu:/homes/liu/Codes/PrefForestLearnExperiments/VehicleDownsampledFurther/Original/StrictExamples/User0/Testing/test0 ./VehicleDownsampledFurther/Original/StrictExamples/User0/Testing/
